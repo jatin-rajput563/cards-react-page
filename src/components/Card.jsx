088e3fdb8bb9269f8adec2c169877be05aa59835
jatin-rajput563 <jkt3298@gmail.com>
2025-04-11 12:18:32 +0530

    final-commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,21 +6,31 @@ const Card = () => {
         <>
             <div className='min-vh-100 justify-content-center align-items-center d-flex flex-column'>
                 <div className='container'>
-                    <div className='row'>
+                    <div className='row flex-wrap'>
                         {IMAGE_DATA.map((item, index) => (
-                            <div className="col-3">
-                                <div key={index} className="card">
-                                    <div className='icon'>
-                                        <FacebookIcon />
-                                    </div>
-                                    <div  className='icon'>
-                                        <InstagramIcon />
-                                    </div>
-                                    <div  className='icon'>
-                                        <TwiterIcon />
-                                    </div>
-                                    <div  className='icon'>
-                                        <SkypeIcon />
+                            <div className="col-lg-3 col-md-4 col-sm-6 mb-20 mx-auto" key={index}>
+                                <div key={index} className="card ">
+                                    <div className='icon-container'>
+                                        <div className='icon'>
+                                            <a href="https://www.facebook.com/" target='_blank'>
+                                                <FacebookIcon />
+                                            </a>
+                                        </div>
+                                        <div className='icon'>
+                                            <a href="https://www.instagram.com/" target='_blank'>
+                                                <InstagramIcon />
+                                            </a>
+                                        </div>
+                                        <div className='icon'>
+                                            <a href="https://x.com/?lang=en" target='_blank'>
+                                                <TwiterIcon />
+                                            </a>
+                                        </div>
+                                        <div className='icon'>
+                                            <a href="https://www.skype.com/en/" target='_blank'>
+                                                <SkypeIcon />
+                                            </a>
+                                        </div>
                                     </div>
                                     <img src={item.Image} alt={item.title} className="card-image max-w-270 w-100" />
                                     <div className="card-content">
